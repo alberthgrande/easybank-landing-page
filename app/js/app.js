f1eb50361be4d6e97779cf257c8aaaeb74a3308e
Alberth G. Ruado <alberthruado@gmail.com>
2023-03-07 20:16:12 +0800

    navigation bar is now functional

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,11 @@
-console.log("test");
+const btnMobileNav = document.querySelector(".btn-mobile-nav");
+const navMain = document.querySelector(".nav-main");
+
+const iconOpen = document.querySelector(".open-icon");
+const iconClose = document.querySelector(".close-icon");
+
+btnMobileNav.addEventListener("click", () => {
+  navMain.classList.toggle("active");
+  iconOpen.classList.toggle("open");
+  iconClose.classList.toggle("open");
+});
